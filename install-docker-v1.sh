ff8d30aef4f01553e3a9449b8e7f634dc35b6d81
natan <natansdj@users.noreply.github.com>
2019-04-24 09:27:16 +0700

    update docker

--- install-docker-v1.sh
+++ install-docker-v1.sh
@@ -28,7 +28,7 @@ sudo apt-get install docker-ce
 
 
 ###Docker Compose
-sudo curl -L https://github.com/docker/compose/releases/download/1.23.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.24.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 ###adding the current user to the docker group:
